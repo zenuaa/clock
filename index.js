c6eed3886b09e4a34ab7179b2519a2af134666dd
zenua <zenuaa@outlook.com>
2024-02-07 21:51:40 +0200

    second clock done

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
 // First clock
 
-const clock = (time) => {
+// const clock = (time) => {
 
-    const currentTime = () => {
-        return new Date().toLocaleTimeString();
-    }
-    const tickTack = () => {
-        const timerId = setInterval(() => {
-            if (time !== 0) {
-                console.clear();
-                console.log(currentTime());
-                time--;
-            }
-            else {
-                clearInterval(timerId)
-            }
-        }, 1000);
-    }
-    tickTack(10);
-}
+//     const currentTime = () => {
+//         return new Date().toLocaleTimeString();
+//     }
+//     const tickTack = () => {
+//         const timerId = setInterval(() => {
+//             if (time !== 0) {
+//                 console.clear();
+//                 console.log(currentTime());
+//                 time--;
+//             }
+//             else {
+//                 clearInterval(timerId)
+//             }
+//         }, 1000);
+//     }
+//     tickTack(10);
+// }
 
-clock(5);
+// clock(5);
 
 // Second clock
+
+const getTime = () => new Date();
+const clear = () => console.clear();
+
+const serializeClockTime = date => ({
+    hours: date.getHours(),
+    minutes: date.getMinutes(),
+    seconds: date.getSeconds()
+});
+
+
+const appendAMPM = clockTime => ({
+    ...clockTime,
+    ampm: clockTime.hours >= 12 ? 'PM' : 'AM'
+})
+
+const civilianHours = timeObj => ({
+    ...timeObj,
+    hours: timeObj.hours > 12 ? timeObj.hours - 12 : timeObj.hours
+});
+
+const formatClock = formatStr => timeObj =>
+    formatStr
+        .replace('hh', timeObj.hours < 10 ? '0' + timeObj.hours : timeObj.hours)
+        .replace('mm', timeObj.minutes < 10 ? '0' + timeObj.minutes : timeObj.minutes)
+        .replace('ss', timeObj.seconds < 10 ? '0' + timeObj.seconds : timeObj.seconds)
+        .replace('tt', timeObj.ampm);
+
+
+const tickTack = (count) => {
+    let i = 0;
+    const intervalId = setInterval(() => {
+        if (i < count) {
+            const timeObj = civilianHours(appendAMPM(serializeClockTime(getTime())));
+            const formattedTime = formatClock('hh:mm:ss tt')(timeObj);
+            clear();
+            console.log(formattedTime);
+            i++;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 1000);
+};
+tickTack(10)

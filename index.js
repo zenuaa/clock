716007fd223a7ac72cd07edaf843ea7cbfe1cd18
zenua <zenuaa@outlook.com>
2024-02-07 16:22:07 +0200

    first clock modified

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 // First clock
-const currentTime = () => {
-    return new Date().toLocaleTimeString();
+
+const clock = (time) => {
+
+    const currentTime = () => {
+        return new Date().toLocaleTimeString();
+    }
+    const tickTack = () => {
+        const timerId = setInterval(() => {
+            if (time !== 0) {
+                console.clear();
+                console.log(currentTime());
+                time--;
+            }
+            else {
+                clearInterval(timerId)
+            }
+        }, 1000);
+    }
+    tickTack(10);
 }
-const tickTack= (time) => {
-    const timerId = setInterval(() => {
-        if (time !== 0) {
-            console.clear();
-            console.log(currentTime());
-            time --;
-        }
-        else {
-            clearInterval(timerId)
-        }
-    }, 1000);
-}
-// tickTack(10);
+
+clock(5);
 
 // Second clock
